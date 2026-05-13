758a72e4e1a8e9fcea07322f9146f0e0da5aa8b4
Jonathan Eduardo <jonathanemjj@gmail.com>
2026-05-13 11:34:28 -0300

    Projeto dashboard blazor

---
+++ models/Grupo.cs
@@ -0,0 +1,10 @@
+namespace RachaAi.Models
+{
+    public class Grupo
+    {
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Valor { get; set; }
+        public bool UsuarioDeve { get; set; }
+    }
+}
